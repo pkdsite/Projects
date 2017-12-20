e8294782b2ab65ce30593850011c27da7fe47d6e
PRADYUMNA KUMAR DALAI <34702615+pkdsite@users.noreply.github.com>
2017-12-20 19:07:10 +0530

    iconbar

---
+++ iconbar.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style type="text/css">
+	body{padding: 0;margin: 0;}
+	.iconbar{width: 100%;background-color: black;height: 50;}
+	.iconbar a{color: white;text-align: center;padding: 10px ;width: 20%;float: left;font-size: 30}
+	.iconbar a:hover{background-color: white;color: black;}
+</style>
+<div class="iconbar">
+	<a href="#" class="active"><span class="fa fa-home"></span></a>
+
+	<a href="#"><span class="fa fa-search"></span></a>
+	<a href="#"><span class="fa fa-globe"></span></a>
+	<a href="#"><span class="fa fa-trash"></span></a>
+</div>
