fa7ab09a60db59b31e34948a4ff8c308049b0797
PRADYUMNA KUMAR DALAI <34702615+pkdsite@users.noreply.github.com>
2017-12-20 12:51:16 +0530

    Fix sidenav

---
+++ sidenavfixed.php
@@ -0,0 +1,26 @@
+<style type="text/css">
+	.side{height: 100%;width: 180;top:0;left:0;background-color: black;position: fixed;}
+	.side a{color: white;font-size: 25px;display: block;padding: 10px;text-decoration: none;}
+	.side a:hover{background-color: orange;color: black;}
+	.active{background-color: orange;color: 
+		black;}
+	.main{margin-left: 180;}
+	@media screen and (max-height:450px){
+		.side a{font-size: 18px;}
+		.main h1{font-size: 22px;}
+
+	}	
+
+</style>
+
+<div class="side">
+	<a href="" class="active">Home</a>
+	<a href="">Services</a>
+	<a href="">About Us!</a>
+	<a href="">Contact Us!</a>
+</div>
+<div class="main">
+	<h1>Hii!! i am pradyumna kumar dalai.</h1>  <p>I want to go to school.
+	I Love my India and love my school very much.
+	Now i become a web developer. Right  now i am playing with the code to design the web and make the web attractive. </p>
+	</div>
